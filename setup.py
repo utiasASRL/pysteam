010fd9bc679d0c8763dec2d4495005274249df74
Yuchen Wu <cheneyuwu@gmail.com>
2021-10-23 01:10:03 -0400

    upgrade version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name="asrl-pysteam",
-    version="1.0.1",
+    version="1.0.2",
     author="Yuchen Wu",
     author_email="cheney.wu@mail.utoronto.ca",
     description="Python implementation of steam",
@@ -18,5 +18,5 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     license="BSD",
     python_requires='>=3.8',
-    install_requires=["numpy>=1.21.0", "scipy>=1.7.0", "matplotlib>=3.4.2", "asrl-pylgmath>=1.0.0"],
+    install_requires=["numpy>=1.21.0", "scipy>=1.7.0", "matplotlib>=3.4.2", "asrl-pylgmath>=1.0.2"],
 )
