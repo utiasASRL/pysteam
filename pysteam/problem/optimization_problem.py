6ff37f28c54c9e453e12400b8fb96656b7505b80
Yuchen Wu <cheneyuwu@gmail.com>
2021-11-10 13:53:09 -0500

    add a customized coo matrix contructor that supports incremental construction while being faster than lil matrix slicing

--- pysteam/problem/optimization_problem.py
+++ pysteam/problem/optimization_problem.py
@@ -1,10 +1,49 @@
 from typing import List, Tuple
-
+import array
 import numpy as np
-from scipy.sparse import lil_matrix
+from scipy.sparse import coo_matrix, dok_matrix
 
 from ..state import StateVar, StateVector
 from . import CostTerm
+
+
+class LazyCOOBuilder:
+
+  class SlicingHandler:
+
+    def __init__(self, key: Tuple[slice, slice]):
+      self.key = key
+
+      rows, cols = key
+      rows = range(rows.stop)[rows]
+      cols = range(cols.stop)[cols]
+      self.rows = np.repeat(rows, len(cols))
+      self.cols = np.tile(cols, len(rows))
+
+      self.data = None
+
+    def __iadd__(self, data: np.ndarray):
+      self.data = data.reshape(-1)
+      return self
+
+  def __init__(self, shape: Tuple[int, int]):
+    self.shape = shape
+    self.rows = array.array('i')
+    self.cols = array.array('i')
+    self.data = array.array('d')
+
+  def tocoo(self):
+    return coo_matrix((self.data, (self.rows, self.cols)), self.shape)
+
+  def __getitem__(self, key):
+    return LazyCOOBuilder.SlicingHandler(key)
+
+  def __setitem__(self, key: Tuple[slice, slice], value: SlicingHandler):
+    assert key == value.key
+    if value.data is not None:
+      self.rows.extend(value.rows)
+      self.cols.extend(value.cols)
+      self.data.extend(value.data)
 
 
 class OptimizationProblem:
@@ -37,8 +76,13 @@ class OptimizationProblem:
   def build_gauss_newton_terms(self, state_vector: StateVector, sparse: bool) -> Tuple[np.ndarray, np.ndarray]:
     """Computes the left-hand approximated Hessian (A) and right-hand gradient vector (b)."""
     state_size = state_vector.get_state_size()
-    A = lil_matrix((state_size, state_size)) if sparse else np.zeros((state_size, state_size))
+    # equivalent to
+    #   A = dok_matrix((state_size, state_size)) if sparse else np.zeros((state_size, state_size))
+    #   ...
+    #   return A.tocsr()
+    # but faster
+    A = LazyCOOBuilder((state_size, state_size)) if sparse else np.zeros((state_size, state_size))
     b = np.zeros((state_size, 1))
     for cost_term in self._cost_terms:
       cost_term.build_gauss_newton_terms(state_vector, A, b)
-    return A.tocsr() if sparse else A, b
+    return A.tocoo().tocsr() if sparse else A, b
