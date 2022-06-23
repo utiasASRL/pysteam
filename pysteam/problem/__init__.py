7d85bcd01b6f775909140f69c83b86b6f1a49b9b
Yuchen Wu <cheneyuwu@gmail.com>
2022-06-23 12:40:08 -0400

    more loss functions

--- pysteam/problem/__init__.py
+++ pysteam/problem/__init__.py
@@ -1,5 +1,5 @@
 from .state_vector import StateVector
 from .noise_model import NoiseModel, StaticNoiseModel, DynamicNoiseModel
-from .loss_func import LossFunc, L2LossFunc
+from .loss_func import LossFunc, L2LossFunc, CauchyLossFunc, DcsLossFunc, GemanMcClureLossFunc
 from .cost_term import CostTerm, WeightedLeastSquareCostTerm
 from .optimization_problem import OptimizationProblem
